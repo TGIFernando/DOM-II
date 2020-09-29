55dc80c068a04e2ebf7444b88444a60736a61455
TGIFernando <fjchavez01@gmail.com>
2020-09-29 13:13:51 -0700

    added 2 event listeners for the buttons on the bottom of the page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-// Your code goes here
+/*
+ * `mouseover`
+ * `keydown`
+ * `wheel`
+ * `load`
+ * `focus`
+ * `resize`
+ * `scroll`
+ * `select`
+ * `dblclick`
+ * `drag / drop`
+ */
+
+let signButtn = document.querySelectorAll('.content-pick .btn');
+signButtn.forEach(bttn => bttn.addEventListener('mouseover', () => {
+    bttn.style.backgroundColor = 'red';
+}));
+signButtn.forEach(bttn => bttn.addEventListener('mouseleave', () => {
+    bttn.style.backgroundColor = '#17A2B8';
+}));
