df983d6fb82affe9631d52b207fe5472873acd1a
TGIFernando <fjchavez01@gmail.com>
2020-09-29 14:40:40 -0700

    added key down effect

--- js/index.js
+++ js/index.js
@@ -14,7 +14,8 @@
 let signButtn = document.querySelectorAll('.content-pick .btn');
 let navLink = document.querySelectorAll('nav a');
 let navBar = document.querySelector('.main-navigation');
-let mainPics = document.querySelectorAll('.img-content')
+let mainPics = document.querySelectorAll('.img-content');
+let mainBody = document.querySelector('body');
 
 //sign in button effects
 signButtn.forEach(bttn => bttn.addEventListener('mouseover', () => {
@@ -32,14 +33,25 @@ navLink.forEach(bttn => bttn.addEventListener('click', (event) => {
 }));
 
 //changing nav bar color
-navBar.addEventListener('click', () => {
+navBar.addEventListener('mousemove', () => {
     navBar.style.backgroundColor = 'blue';
 });
+navBar.addEventListener('mouseleave', () => {
+    navBar.style.backgroundColor = "white";
+});
 
-//hover effects for pics
-mainPics.forEach(pic => pic.addEventListener('mouseover', () => {
+//click effects for pics
+mainPics.forEach(pic => pic.addEventListener('dblclick', () => {
     pic.style.opacity = '50%';
+    pic.style.border = '2px solid red';
+    pic.style.borderRadius = '10px';
 }));
 mainPics.forEach(pic => pic.addEventListener('mouseleave', () => {
     pic.style.opacity = '100%';
+    pic.style.border = 'none';
 }));
+
+//key down to change bg color of page
+mainBody.addEventListener('keydown', () => {
+    mainBody.style.backgroundColor = '#0099cc';
+});
